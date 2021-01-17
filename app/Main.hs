e20908e8207ca3b1e1752714f99550f280682005
Charanjit Singh <ckhabra@gmail.com>
2021-01-17 13:48:21 +0530

    feat: basic CLI using optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
 module Main where
 
-import qualified SaunfMain as Saunf
+import Options.Applicative
+
+data CliOptions = Readme ReadmeOptions deriving (Show)
+
+data ReadmeOptions
+  = Push -- Push changes to readme
+  | Pull -- Pull changes back from readme
+  deriving (Show)
+
+readmeOptions :: Parser ReadmeOptions
+readmeOptions =
+  subparser
+    ( command "push" (info (pure Push) (progDesc "Push changes to readme.md"))
+        <> command "pull" (info (pure Pull) (progDesc "Pull changes made to readme.md back"))
+    )
+
+cliOptions :: Parser CliOptions
+cliOptions =
+  subparser $ command "readme" (info (Readme <$> readmeOptions <**> helper) (progDesc "Manage the readme file"))
+
+cliParser :: ParserInfo CliOptions
+cliParser = info (cliOptions <**> helper) (fullDesc <> header "Tasty project management" <> progDesc "Manage software projects with plain-text")
 
 main :: IO ()
-main = Saunf.main
+main = do
+  val <- execParser cliParser
+  case val of
+    Readme Push -> putStrLn "beep boop...(re)creating readme.md...boop beep"
+    _ -> putStrLn "Not implemented yet :-("
