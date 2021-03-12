7367ced4a9bb88d1ce09a88915b425f9e6571ebd
Charanjit Singh <ckhabra@gmail.com>
2021-03-12 21:56:31 +0530

    feat: finish issues which retrieves all issues
    Every section which has "CATEGRY: issue" property, and every sub-section of
    every section with "CATEGORY: issues" property is an issue.
    
    Don't know why I went for CUSTOM_ID for saunf-conf; should probably have been
    "CATEGORY: conf"
    
    And guess what, I had finished filterSections last time. Dunno why it kept
    returning a single section for a while, and then just returned all of them
    without any changes in env or code.

--- src/Saunf/Issue.hs
+++ src/Saunf/Issue.hs
@@ -1,16 +1,36 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Saunf.Issue where
 
 import Control.Monad.Reader
 import Relude
-import Saunf.Types
 import Saunf.Shared (filterSections)
+import Saunf.Types
+import qualified Text.Pandoc as P
 
-hasCategory cat block | trace ("HAS CATEGORY: " ++ show block) False = undefined
-hasCategory cat block = True
+hasCategory :: Text -> P.Block -> Bool
+hasCategory cat block = case block of
+  (P.Header _ (_, _, props) _) -> any (\p -> fst p == "category" && snd p == cat) props
+  _ -> True
+
+isHeaderWithLevel :: Int -> P.Block -> Bool
+isHeaderWithLevel lvl block = case block of
+  (P.Header lvl' _ _) -> lvl == lvl'
+  _ -> False
 
 issues :: Reader SaunfEnv [Issue]
 issues = do
+  env <- ask
+  containers <- filterSections (hasCategory "issues")
+  let containerSections = concat $ flip runReader env . issuesFromContainer <$> containers
   sections <- filterSections (hasCategory "issue")
-  return $ Issue <$> sections
+  return $ Issue <$> sections ++ containerSections
+  where
+    issuesFromContainer :: Section -> Reader SaunfEnv [Section]
+    issuesFromContainer = \case
+      (Section (P.Header lvl _ _) body) ->
+        local (\env -> env {saunfDoc = P.Pandoc mempty body}) $
+          filterSections (isHeaderWithLevel (lvl + 1))
+      _ -> return []
--- test/IssueSpec.hs
+++ test/IssueSpec.hs
@@ -6,8 +6,8 @@ module IssueSpec where
 
 import Relude
 import Saunf.Issue
-import Saunf.Types
 import Saunf.Shared
+import Saunf.Types
 import Test.Hspec
 import Text.Pandoc as P
 
@@ -52,4 +52,28 @@ spec = do
       result `shouldBe` expected
 
     it "returns all sub-sections of sections which have property CATEGORY=issues" $ do
-      pending
+      orgFile <-
+        P.handleError
+          =<< P.runIO
+            ( readOrg
+                def
+                "* Conf\n\
+                \:PROPERTIES:\n\
+                \:CUSTOM_ID: saunf-conf\n\
+                \:END:\n\
+                \* Issues\n\
+                \:PROPERTIES:\n\
+                \:CATEGORY: issues\n\
+                \:END:\n\
+                \** Issue 1\n\
+                \** Issue 2\n"
+            )
+      let env = SaunfEnv orgFile mempty
+      let result = (\(Issue s) -> s) <$> runReader issues env
+
+      let expected =
+            [ Section {sectionTitle = Header 2 ("", [], []) [Str "Issue", Space, Str "1"], sectionBody = []},
+              Section {sectionTitle = Header 2 ("", [], []) [Str "Issue", Space, Str "2"], sectionBody = []}
+            ]
+
+      result `shouldBe` expected
