962c625c5b5194c4c8bba278b1cad15813c4c5b0
Charanjit Singh <ckhabra@gmail.com>
2021-04-03 17:08:09 +0530

    chore: add default GITHUB_TOKEN env variable in dev-shell
    When I open saunf.dhall in Emacs, dhall-mode punch me in the face if this is not
    present.
    
    Also,
    - chore: format flake.nix using nixpkg-fmt

--- flake.nix
+++ flake.nix
@@ -9,46 +9,47 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-  let
-    overlay = self: super: {
-      haskellPackages = super.haskellPackages.override {
-        overrides = hself: hsuper: {
-          saunf =
-            hself.callCabal2nix
-            "saunf"
-            (self.nix-gitignore.gitignoreSourcePure
-            [ ./.gitignore "flake.nix" ]
-            ./.
-            )
-            { };
+    let
+      overlay = self: super: {
+        haskellPackages = super.haskellPackages.override {
+          overrides = hself: hsuper: {
+            saunf =
+              hself.callCabal2nix
+                "saunf"
+                (self.nix-gitignore.gitignoreSourcePure
+                  [ ./.gitignore "flake.nix" ]
+                  ./.
+                )
+                { };
+          };
         };
+        saunf =
+          self.haskell.lib.justStaticExecutables
+            self.haskellPackages.saunf;
       };
-      saunf =
-        self.haskell.lib.justStaticExecutables
-        self.haskellPackages.saunf;
-    };
-  in
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; config = { allowBroken = true; }; };
-  in
-  {
-    defaultPackage = pkgs.saunf;
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; config = { allowBroken = true; }; };
+      in
+      {
+        defaultPackage = pkgs.saunf;
 
-    devShell = pkgs.haskellPackages.shellFor {
-      packages = p: [
-        p.saunf
-      ];
-      buildInputs = with pkgs.haskellPackages; [
-        ghc
-        cabal-install
-        hlint
-        pkgs.nixpkgs-fmt
-        pkgs.ormolu
-        pkgs.dhall
-      ];
-      withHoogle = false;
-    };
-  }
-  );
+        devShell = pkgs.haskellPackages.shellFor {
+          packages = p: [
+            p.saunf
+          ];
+          buildInputs = with pkgs.haskellPackages; [
+            ghc
+            cabal-install
+            hlint
+            pkgs.nixpkgs-fmt
+            pkgs.ormolu
+            pkgs.dhall
+          ];
+          withHoogle = false;
+          GITHUB_TOKEN = "DEFAULT_FROM_FLAKE";
+        };
+      }
+    );
 }
