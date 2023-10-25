166708c5c3bcba04a05039b494204d5000f5dc97
Charanjit Singh <bitspook@proton.me>
2023-10-25 06:51:54 +0200

    chore: elisp variable
    Apparently Elpaca needs a commit

--- src/saunf.el
+++ src/saunf.el
@@ -1,5 +1,11 @@
 ;;; saunf --- Personal project manager -*- lexical-binding: t; -*-
 ;;;
+;; Author: bitspook
+;; Homepage: https://github.com/bitspook/saunf
+;; Keywords: project-management
+;; Version: 0.0.1
+;; Package-Requires: ((sly "1.0.43"))
+;;;
 ;;; Commentary:
 ;;; Saunf is a personal-project manager. Its purpose is to help you exercise
 ;;; your personal freedom for project management, while allowing your team and
@@ -7,10 +13,6 @@
 ;;;
 ;;; Code:
 (require 'sly)
-
-(defun saunf--insert-org-link (url description)
-  "Insert a new org-link to URL with DESCRIPTION."
-  (insert (format "[[%s][%s]]" url description)))
 
 (defun saunf--cl-eval (cl-code)
   "Eval common-lisp CL-CODE."
@@ -23,6 +25,11 @@
       (cl:eval
        (cl:read-from-string
         ,(prin1-to-string cl-code)))))))
+
+(defvar saunf-projects '()
+  "List of active projects. An active project is a symbol representing a live saunf-project in CL.")
+
+(message "SAUNF IS LOADING")
 
 (provide 'saunf)
 ;;; saunf.el ends here
